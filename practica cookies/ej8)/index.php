3b6a12becdd29ffbc5951cde247641332c0ae5e7
JoaquinGim <143027720+JoaquinGim@users.noreply.github.com>
2023-11-01 11:29:36 -0300

    Create index.php

---
+++ practica cookies/ej8)/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Buscador de Canciones</title>
+</head>
+<body>
+  <h1>Buscador de Canciones</h1>
+
+  <form action="buscar_canciones.php" method="POST">
+    <label for="cancion">Buscar canción:</label>
+    <input type="text" id="cancion" name="cancion" required>
+    <button type="submit">Buscar</button>
+  </form>
+</body>
+</html>
